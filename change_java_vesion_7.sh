e6a940292f588476a04f47acc7593fe682658e56
par <par@logpoint.com>
2020-07-01 15:22:57 +0545

    change in port

--- change_java_vesion_7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-JAVA_HOME=/app/oracle/jdk1.7.0_76
-export JAVA_HOME
-export PATH=${JAVA_HOME}/bin:${PATH}
--- change_java_vesion_8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-JAVA_HOME=/app/oracle/jdk1.8.0_121
-export JAVA_HOME
-export PATH=${JAVA_HOME}/bin:${PATH}
